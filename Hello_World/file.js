160516a92b7164232ff2dadbbf06e7d458a7a5e2
cs-Deepak <d.deepakkumaryadav9162@gamil.com>
2025-08-29 00:20:15 +0530

    first commit

---
+++ Hello_World/file.js
@@ -0,0 +1,21 @@
+const { isUtf8 } = require('buffer')
+const fs = require('fs')
+
+//sync....
+// fs.writeFileSync('./test.txt', "Hello world I'm Deepak")
+
+
+// Async....
+// fs.writeFile('./test.txt', "Hello world I'm Deepak. Async..", (err) => {})
+
+// const result =    fs.readFileSync('./contacts.txt', "utf-8" );
+// console.log(result)
+
+// fs.readFile('./contacts.txt', "utf-8", (err, result)=>{
+//     if(err) {
+//         console.log("Error", err);
+//     } else {
+//         console.log(result);
+//     }
+// })
+
---
+++ Hello_World/hello.js
@@ -0,0 +1,3 @@
+const {add, sub} = require ("./math")
+
+console.log("Math valu is ", add(2,5), sub(5,3))
